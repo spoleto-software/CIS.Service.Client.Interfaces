dd590b7b7dac0079338b170e2043ef94c6ad0faf
o.nadymov <o.nadymov@spoleto.ru>
2023-06-08 21:35:02 +0300

    Just to activate github action

--- src/CIS.Service.Client.Interfaces/IBody.cs
+++ src/CIS.Service.Client.Interfaces/IBody.cs
@@ -1,7 +1,7 @@
 ﻿namespace CIS.Service.Client.Interfaces
 {
     /// <summary>
-    /// Interface for the body of CIS.Service.Client.ContainerModel{T}. 
+    /// Interface for the body of CIS.Service.Client.ContainerModel{T}.
     /// </summary>
     public interface IBody
     {
